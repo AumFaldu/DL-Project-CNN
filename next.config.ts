9961b2a7b3f38fcf1de34ecb85a9510418e07668
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2026-03-21 17:57:26 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  turbopack: {
-    root: "/Users/aum/Developer/DL-Project/frontend",
-  },
 };
 
 export default nextConfig;
