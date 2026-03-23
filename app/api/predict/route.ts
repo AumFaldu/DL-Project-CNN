3164381c6bf2fdf6b24b7dcf2077a73b8473e222
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2026-03-23 22:12:24 +0530

    Update route.ts

--- app/api/predict/route.ts
+++ app/api/predict/route.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic";
-
 export async function POST(req: NextRequest) {
   const formData = await req.formData();
   const image = formData.get("image");
